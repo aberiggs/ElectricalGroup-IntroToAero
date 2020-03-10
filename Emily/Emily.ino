3df77685b30fcde044644931143edfbd0efab99d
Flyguy52 <61603764+Flyguy52@users.noreply.github.com>
2020-03-10 14:23:57 -0700

    Update Emily.ino

--- Emily/Emily.ino
+++ Emily/Emily.ino
@@ -1,11 +1,44 @@
 // Emily
+#include<Wire.h>
+int LED = 0
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
-void setup() {
-  // put your setup code here, to run once:
-
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+pinMode (LED, OUTPUT);
+  
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  int LastX = AcX
+  
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+  
+  Serial.print("Accelerometer: ");
+  Serial.print("X = "); Serial.print(AcX);
+  Serial.print(" | Y = "); Serial.print(AcY);
+  Serial.print(" | Z = "); Serial.println(AcZ); 
+  
+  Serial.print("Gyroscope: ");
+  Serial.print("X = "); Serial.print(GyX);
+  Serial.print(" | Y = "); Serial.print(GyY);
+  Serial.print(" | Z = "); Serial.println(GyZ);
+  Serial.println(" ");
+  Serial.println(LastX); 
+  delay(333);
+}
 }
